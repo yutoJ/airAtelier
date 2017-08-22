e8cdf4e065579bc1e16a3644318e351a5ef5cbb8
yutoJ <yutoogata4@gmail.com>
2017-08-22 16:48:45 +0900

    put reservation on host calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -7,15 +7,18 @@ class CalendarsController < ApplicationController
     params[:start_date] ||= Date.current.to_s
     params[:room_id] ||= @rooms[0] ? @rooms[0].id : nil
 
+
+
     if params[:room_id]
-      @room = Romom.find(params[:room_id])
+      @room = Room.find(params[:room_id])
       start_date = Date.parse(params[:start_date])
 
       first_of_month = (start_date - 1.months).beginning_of_month
       end_of_month = (start_date + 1.months).end_of_month
 
-      @events = @room.reservations.join(:user)
-                      .select('reservations.*, users.fullname, users.image, users.email, users.uid')
+      @events = @room.reservations.joins(:user)
+                      .select('reservations.*, users.fullname, users.email')
+                      .select('reservations.*, users.fullname,, users.image, users.email, users.uid')
                       .where('(start_date BETWEEN ? AND ?) AND status <> ?', first_of_month, end_of_month, 2)
     else
       @room = nil
